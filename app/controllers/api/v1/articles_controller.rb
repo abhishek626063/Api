bb862889697896d06e80ee29259d6d2dd3670951
Harshit <bhawsarharshit@gmail.com>
2023-01-16 20:21:39 +0530

    firstthanck

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,63 @@
+class Api::V1::ArticlesController < ApplicationController
+  def index
+    articles = Article.all
+    render json: articles, status: 200
+  end
+
+  def show
+    article = Article.find_by(id: params[:id])
+    if article
+      render json: article, status: 200
+    else
+      render json: {
+        error: "Article not found"
+      }
+    end
+  end
+
+  def create
+    article = Article.new(
+      title: arti_params[:title],
+      body: arti_params[:body],
+      name: arti_params[:name]
+    )
+    if article.save
+      render json: article, status: 200
+    else
+      render json: {
+        error: "Error Creating"
+      }
+  end
+end
+
+  def update
+    article = Article.find_by(id: params[:id])
+    if article
+      article.update(title: arti_params[:title],body: arti_params[:body],name: arti_params[:name])
+      render json: "Article update successfully"
+    else  
+       render json: {
+        error: "Article not found"
+
+      }
+    end
+  end
+
+  def destroy
+    article = Article.find_by(id: params[:id])
+    if article
+      article.destroy
+      render json: "Article is the deleted"
+    end
+
+  end
+
+  private
+  def arti_params
+    params.require(:article).permit([
+      :title,
+      :body,
+      :name
+    ])
+  end
+end
